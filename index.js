f6549d16c77dd293b4801df02c4175d25d204e0b
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-01-07 01:06:36 +0900

    Add static folder

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 
 const port = 3000
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
